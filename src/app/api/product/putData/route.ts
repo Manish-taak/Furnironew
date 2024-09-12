b04425121acd62d45ddce6eda1cc47e03d5a139f
sahil soni <sonisahil1018@gmail.com>
2024-09-12 12:20:54 +0530

    push update

--- src/app/api/product/putData/route.ts
+++ src/app/api/product/putData/route.ts
@@ -1,15 +1,29 @@
 import prisma from "@/lib";
 import { NextRequest, NextResponse } from "next/server";
+import { number } from "zod";
 
 export async function PUT(req: NextRequest) {
 try {
     const url = new URL(req.url);
     const id = url.searchParams.get("id");
     const data = await req.json();
+
+    const productId = Number(id);
+
     if (!id) {
-        return NextResponse.json({ success: false, error: "Product ID is required" }, { status: 400 });
+        return NextResponse.json({ success: false, error: "Product ID is required", status:400 });
     }
 
+    if(isNaN(productId)){
+        return NextResponse.json({ success: false, error: "Product ID is invalid" , status:400 });
+    }
+
+    const existingProduct = await prisma.product.findUnique({where: { id: productId },});
+  
+      if (!existingProduct) {
+        return NextResponse.json({ error: 'Product with this ID does not exist' , status:400 });
+      }
+  
     const updateProduct = await prisma.product.update({where:{id: Number(id)}, 
         data: {
             title: data.title,
@@ -31,7 +45,6 @@ try {
                 })),
             },
         },
-        
     })
     return NextResponse.json({ success: true, message: "Product updated successfully", product: updateProduct }, { status: 200 });
 } catch (error) {

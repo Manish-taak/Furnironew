2d6994d1df906d7931d09c81971cbd0c0c484313
Manish-taak <manish05.mdb@gmail.com>
2024-12-24 17:56:24 +0530

    update pull

---
+++ src/app/api-doc/page.tsx
@@ -0,0 +1,14 @@
+import { getApiDocs } from "../../lib/Swagger"
+
+import ReactSwagger from './react-swagger';
+
+export default async function IndexPage() {
+    const spec = await getApiDocs();
+
+    return (
+        <section className="container">
+            <ReactSwagger spec={spec} />
+        </section>
+    );
+}
+

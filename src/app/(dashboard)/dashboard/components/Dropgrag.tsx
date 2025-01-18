69d587ae9f6d5067842d8b515c4b617393ba0a39
manish-taak <manishtaak00010@gmail.com>
2025-01-18 12:53:00 +0530

    drop and drag component add

--- src/app/(dashboard)/dashboard/components/Dropgrag.tsx
+++ src/app/(dashboard)/dashboard/components/Dropgrag.tsx
@@ -40,7 +40,7 @@ const DragAndDrop: React.FC = () => {
 
     return (
         <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-            {items.map((item) => (
+            {items && items?.map((item) => (
                 <div
                     key={item.id}
                     id={`item-${item.id}`}
